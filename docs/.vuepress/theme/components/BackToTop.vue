15bb004fb4981391dc88aa66d66b67d25a899912
Zé Bateira <jose.bateira@protonmail.com>
2021-07-20 10:26:35 +0100

    fix: ui fixes and tweaks
    * fix: missing on click handler for back to top arrow
    
    * fix: stick footer to bottom by forcing content to grow
    
    * fix: back to top transitions and positioning
    
    * fix: always show main scrollbar (hover not working sometimes)
    
    * fix: fade out last updated text
    
    * fix: prevent back to top arrow to show on top of footer
    
    * fix: force back to top to show above all elements
    
    * fix: search input text
    
    * fix: back to top position
    
    * fix: search dark theme

--- docs/.vuepress/theme/components/BackToTop.vue
+++ docs/.vuepress/theme/components/BackToTop.vue
@@ -1,16 +1,18 @@
 <template>
-  <transition name="fade">
+  <div
+      class="back-to-top"
+      :class="{ show }"
+    >
     <svg
-        v-if="show"
-        class="back-to-top"
         fill="currentColor"
         xmlns="http://www.w3.org/2000/svg"
+        @click="scrollToTop"
         viewBox="0 0 49 28"
     >
       <path d="M23.334 1.414l-21.92 21.92a2 2 0 000 2.829l.707.707a2 2 0 002.829 0L26.87 4.95a2 2 0 000-2.829l-.707-.707a2 2 0 00-2.829 0z" />
       <path d="M22.615 4.95l21.92 21.92a2 2 0 002.829 0l.707-.708a2 2 0 000-2.828L26.15 1.414a2 2 0 00-2.83 0l-.706.707a2 2 0 000 2.828z" />
     </svg>
-  </transition>
+  </div>
 </template>
 
 <script>
@@ -56,25 +58,14 @@ export default {
 <style lang='stylus' scoped>
 .back-to-top {
   position: sticky;
+  z-index: 5;
   opacity: 0.7;
+  height: 0;
 
   cursor: pointer;
   align-self: flex-end;
-
-  --inner-size: 2.5rem;
-  --spacing: calc(var(--inner-size) * 0.25);
-  --size: calc(var(--inner-size) - var(--spacing) * 2);
-
   bottom: 2rem;
   right: 0;
-  transform: translateX(-100%);
-
-  height: var(--size);
-  width: var(--size);
-  padding: var(--spacing);
-
-  color: $arrowColor;
-  background-color: $arrowBgColor;
 
   transition: opacity 0.3s;
 }
@@ -83,17 +74,34 @@ export default {
   opacity: 1;
 }
 
+.back-to-top svg {
+  --inner-size: 2.5rem;
+  --spacing: calc(var(--inner-size) * 0.25);
+  --size: calc(var(--inner-size) - var(--spacing) * 2);
+
+  height: var(--size);
+  width: var(--size);
+  padding: var(--spacing);
+  
+  background-color: $arrowBgColor;
+  color: $arrowColor;
+  opacity: 0;
+
+  transform: translate(-100%, 100%);
+
+  transition:
+    transform 0.2s,
+    opacity 0.2s;
+}
+
 @media (max-width: 959px) {
   .back-to-top {
     display: none;
   }
 }
 
-.fade-enter-active, .fade-leave-active {
-  transition: transform 0.2s;
-}
-
-.fade-enter, .fade-leave-to {
-  transform: translateX(-100%) translateY(150%);
+.back-to-top.show svg {
+  transform: translate(-100%, -100%);
+  opacity: 1;
 }
 </style>
